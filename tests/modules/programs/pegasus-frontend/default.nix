a551619bfbcd0f9bbf72a439f43d85e27631db97
Meow <github@xela.codes>
2026-03-03 14:43:53 -0500

    test for favorites

--- tests/modules/programs/pegasus-frontend/default.nix
+++ tests/modules/programs/pegasus-frontend/default.nix
@@ -2,5 +2,6 @@
   pegasus-frontend-basic-configuration = ./basic-configuration.nix;
   pegasus-frontend-collections-configuration = ./collections-configuration.nix;
   pegasus-frontend-collectionMerge-configuration = ./collectionMerge-configuration.nix;
+  pegasus-frontend-favorites-configuration = ./favorites-configuration.nix;
   pegasus-frontend-theme-configuration = ./theme-configuration.nix;
 }
