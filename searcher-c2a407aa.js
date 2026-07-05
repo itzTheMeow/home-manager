7bbc44cf91d769dbd46a77bdb93d218a4ae14a49
itzTheMeow <itzTheMeow@users.noreply.github.com>
2026-07-05 17:43:59 +0000

    deploy: 110c3cb5c06bce6a2cc80f1579b250cd9d135329

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2d061fa6.js',
+                path_to_root + 'searchindex-705f6646.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
